527594e4d324fc1bb322e226e34aadf256d7439e
David Villalobos <david.v.valdez@gmail.com>
2016-02-28 23:17:46 -0600

    first steps of login service

--- admin/js/admin.js
+++ admin/js/admin.js
@@ -1,18 +1,25 @@
 (function(){
     'use strict';
     var user,
-    meanBlog = angular.module('meanBlog',['ngRoute','meanBlogCtrls']).config(['$routeProvider',function($routeProvider){
-        $routeProvider.when('/',{
-            templateUrl:'./templates/index.html',
-            controller:'indexCtrl'
-        }).otherwise({
-            redirectTo:'/'
-        });
-    }]),
-    ctrls = angular.module('meanBlogCtrls',[]).controller('indexCtrl',[function(){
-        var ctrl = this;
-        ctrl.checkUser = function(){
-            return true;
-        };
-    }]);
+    meanBlog = angular.module('meanBlog',['ngRoute','meanBlogCtrls'])
+        .config(['$routeProvider',function($routeProvider){
+            $routeProvider.when('/',{
+                templateUrl:'./templates/index.html',
+                controller:'indexCtrl'
+            }).otherwise({
+                redirectTo:'/'
+            });
+        }]),
+    ctrls = angular.module('meanBlogCtrls',['ngResource'])
+        .factory('userSrvc',['$resource',function($resource){
+            return $resource('./gato-:gatoName.json',{},{
+                query:{method:'GET',params:{gatoName:'gato'},isArray:true}
+            });
+        }])
+        .controller('indexCtrl',['userSrvc',function(){
+            var ctrl = this;
+            ctrl.checkUser = function(){
+                return true;
+            };
+        }]);
 }());
