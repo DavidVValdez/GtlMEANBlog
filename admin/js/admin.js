6d221883d5787cb247ff3f5626ab3d6a2a54ba5d
David Villalobos <david.v.valdez@gmail.com>
2016-03-13 17:54:50 -0600

    react list working

--- admin/js/admin.js
+++ admin/js/admin.js
@@ -19151,36 +19151,38 @@ module.exports = require('./lib/React');
         Xhr = require('/web/gtl/v04.20.00/gtl/js/xhr.js'),
         React = require('react'),
         ReactDOM = require('react-dom'),
+        user,
         List = React.createClass({
         displayName: 'List',
 
         xhr: new XMLHttpRequest(),
         getInitialState: function () {
-            return {};
+            return { articles: [] };
+        },
+        parseArticles: function () {
+            return this.state.articles.map(function (item, indx) {
+                return React.createElement(
+                    'div',
+                    { key: indx },
+                    item.title
+                );
+            });
         },
         componentDidMount: function () {
-            console.log('keso');
-        },
-        onClickList: function (evt) {
-            console.log('list');
-        },
-        onClickAdd: function (evt) {
-            console.log('add');
+            this.xhr.onreadystatechange = () => {
+                if (this.xhr.readyState === 4) {
+                    this.setState({ articles: JSON.parse(this.xhr.responseText) });
+                }
+            };
+            this.xhr.open('GET', '/api/articles');
+            this.xhr.setRequestHeader('Authorization', 'Bearer ' + user.token);
+            this.xhr.send('');
         },
         render: function () {
             return React.createElement(
                 'div',
                 null,
-                React.createElement(
-                    'button',
-                    { onClick: this.onClickList },
-                    'List'
-                ),
-                React.createElement(
-                    'button',
-                    { onClick: this.onClickAdd },
-                    'Add'
-                )
+                this.parseArticles()
             );
         }
     }),
@@ -19190,10 +19192,35 @@ module.exports = require('./lib/React');
         getInitialState: function () {
             return { tab: 'list' };
         },
+        switchUI: function (ui) {
+            this.setState({ tab: ui });
+        },
+        onClickList: function (evt) {
+            console.log('list');
+            this.switchUI('list');
+        },
+        onClickAdd: function (evt) {
+            console.log('add');
+            this.switchUI('add');
+        },
         render: function () {
             return React.createElement(
                 'div',
                 null,
+                React.createElement(
+                    'div',
+                    null,
+                    React.createElement(
+                        'button',
+                        { onClick: this.onClickList },
+                        'List'
+                    ),
+                    React.createElement(
+                        'button',
+                        { onClick: this.onClickAdd },
+                        'Add'
+                    )
+                ),
                 (() => {
                     return this.state.tab === 'list' ? React.createElement(List, null) : React.createElement(
                         'div',
@@ -19210,6 +19237,7 @@ module.exports = require('./lib/React');
 
         xhr.onreadystatechange = function () {
             if (xhr.readyState === 4 && xhr.status !== 401) {
+                user = JSON.parse(xhr.responseText);
                 ReactDOM.render(React.createElement(Client, null), gtlQ('body > main'));
             }
         };
--- react/admin.js
+++ react/admin.js
@@ -6,26 +6,33 @@
     React = require('react'),
     ReactDOM = require('react-dom'),
 
+    user,
+
     List = React.createClass({
         xhr:new XMLHttpRequest(),
         getInitialState:function(){
-            return {};
+            return {articles:[]};
+        },
+        parseArticles:function(){
+            return this.state.articles.map(function(item,indx){
+                return (
+                        <div key={indx}>{item.title}</div>
+                );
+            });
         },
         componentDidMount:function(){
-            console.log('keso');
-        },
-        onClickList:function(evt){
-            console.log('list');
-        },
-        onClickAdd:function(evt){
-            console.log('add');
+            this.xhr.onreadystatechange = () => {
+                if(this.xhr.readyState === 4){
+                    this.setState({articles:JSON.parse(this.xhr.responseText)});
+                }
+            };
+            this.xhr.open('GET','/api/articles');
+            this.xhr.setRequestHeader('Authorization','Bearer '+user.token);
+            this.xhr.send('');
         },
         render:function(){
             return (
-                <div>
-                    <button onClick={this.onClickList}>List</button>
-                    <button onClick={this.onClickAdd}>Add</button>
-                </div>
+                <div>{this.parseArticles()}</div>
             );
         }
     }),
@@ -34,12 +41,27 @@
         getInitialState:function(){
             return {tab:'list'};
         },
+        switchUI:function(ui){
+            this.setState({tab:ui});
+        },
+        onClickList:function(evt){
+            console.log('list');
+            this.switchUI('list');
+        },
+        onClickAdd:function(evt){
+            console.log('add');
+            this.switchUI('add');
+        },
         render:function(){
             return (
                 <div>
-                {(() => {
-                    return this.state.tab === 'list' ? <List /> : <div>form</div>
-                })()}
+                    <div>
+                        <button onClick={this.onClickList}>List</button>
+                        <button onClick={this.onClickAdd}>Add</button>
+                    </div>
+                    {(() => {
+                        return this.state.tab === 'list' ? <List /> : <div>form</div>
+                    })()}
                 </div>
             );
         }
@@ -50,6 +72,7 @@
         
         xhr.onreadystatechange = function(){
             if(xhr.readyState === 4 && xhr.status !== 401){
+                user = JSON.parse(xhr.responseText);
                 ReactDOM.render(<Client />, gtlQ('body > main'));
             }
         };
